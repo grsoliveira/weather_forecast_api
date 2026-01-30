6c1ecf455d745c2d09ef00f83e191df9705e7863
Gleibson Oliveira <gleibson.rodrigo>
2026-01-30 11:09:56 -0300

    Initial commit

---
+++ src/main/java/com/olixcorp/weather_forecast_api/WeatherForecastApiApplication.java
@@ -0,0 +1,13 @@
+package com.olixcorp.weather_forecast_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherForecastApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/olixcorp/weather_forecast_api/WeatherForecastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olixcorp.weather_forecast_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
